6c3af05a814ab8d70852bf777b75b253be8041e2
PARULCODE12 <109349527+PARULCODE12@users.noreply.github.com>
2023-04-22 01:48:31 +0530

    Time: 1 ms (100.00%), Space: 39.9 MB (67.02%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        int start = 1;
+        int end = x;
+        int res = 0;
+        int mid;
+        while(start<=end){
+            mid=start+(end-start)/2;
+            if(mid<=x/mid){
+                start=mid+1;
+                res=mid;   
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return res;
+    }
+}
