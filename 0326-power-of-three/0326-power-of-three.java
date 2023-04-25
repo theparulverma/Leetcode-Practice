86f5874d452bbc82bbd916fee35a62c1db1d7969
PARULCODE12 <109349527+PARULCODE12@users.noreply.github.com>
2023-04-25 23:24:43 +0530

    Time: 15 ms (99.73%), Space: 42.6 MB (27.05%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        int i;
+        if(n==0||n==-1){
+            return false;
+        }
+        while(n>=3){
+            if(n%3 !=0)return false;
+            n=n/3;
+        }  
+        return n==1;
+    }
+}
