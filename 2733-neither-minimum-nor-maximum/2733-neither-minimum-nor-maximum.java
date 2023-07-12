38c8eee0f080a4b8d8b6d5be725cb3db65585bb3
PARULCODE12 <109349527+PARULCODE12@users.noreply.github.com>
2023-07-13 00:44:57 +0530

    Time: 13 ms (56.79%), Space: 43.6 MB (91.42%) - LeetHub

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.java
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.java
@@ -4,7 +4,7 @@ class Solution {
         if(len <= 2){//it means array mein sirf 2 elements hai toh maximum or minium dono honge beech ka koi value nhi hogaa soo yeh hamari base case ho jayegii
             return -1;
         }
-        Arrays.sort(nums);
+        Arrays.sort(nums);//sort karke fayda hoga ki maximum or minimum nikalne nhi padege
         return nums[1];
     }
 }
