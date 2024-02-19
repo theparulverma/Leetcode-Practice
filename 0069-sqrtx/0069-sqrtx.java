ec480c97075363f22ea5c54e8547bbc581c0f7b8
theparulverma <109349527+theparulverma@users.noreply.github.com>
2024-02-19 15:18:41 +0530

    Time: 1 ms (87.46%), Space: 40.4 MB (93.22%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,14 +1,14 @@
 class Solution {
     public int mySqrt(int x) {
-        int start = 1;
-        int end = x;
-        int res = 0;
+        int start =1;
+        int end =x;
         int mid;
+        int res=0;
         while(start<=end){
-            mid=start+(end-start)/2;
-            if(mid<=x/mid){
-                start=mid+1;
-                res=mid;   
+            mid=(start+end)/2;//2
+            if(mid <= x/mid){//2<=2
+                start=mid+1;//3
+                res=mid;//to stor th3 output in this i.e 2
             }
             else{
                 end=mid-1;
